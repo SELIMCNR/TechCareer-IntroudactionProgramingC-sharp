0d0faa7a1b77e9ed651a3bbb938caf136b97d915
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-04-06 16:45:16 +0300

    Add files via upload

---
+++ C#/dorduncuders/20230405/01_Switch_Case/Program.cs
@@ -0,0 +1,87 @@
+﻿int firstNumber = 10;
+int secondNumber = 20;
+byte operation;
+Console.WriteLine("Toplama için: 1 , Çıkarma için : 2 , Çarpma için : 3 ,Bölme için : 4");
+
+operation = byte.Parse( Console.ReadLine());
+
+#region ifelse
+if (operation == 1)
+{
+    Console.WriteLine(firstNumber + secondNumber);
+}
+else if(operation == 2)
+{
+    Console.WriteLine(firstNumber - secondNumber);
+}
+else if(operation == 3)
+{
+    Console.WriteLine(firstNumber * secondNumber);
+}
+else if(operation == 4)
+{
+    Console.WriteLine(firstNumber / secondNumber);
+}
+Console.ReadKey();
+#endregion
+switch (operation)
+{
+    case 1:
+        Console.WriteLine(firstNumber+secondNumber);
+        break;
+    case 2:
+        Console.WriteLine(firstNumber- secondNumber);
+        break;
+    case 3:
+        Console.WriteLine(firstNumber*secondNumber);
+        break;
+    case 4:
+        Console.WriteLine(firstNumber/secondNumber);
+        break;
+    default:
+        Console.WriteLine("Hatalı işlem");
+        break;
+}
+
+switch (operation)
+{
+    case 1:
+    case 2:
+        Console.WriteLine(firstNumber + secondNumber);
+            break;
+    case 3:
+    case 4:
+    case 5:
+        break;
+}
+
+ConsoleKeyInfo keyInfo = Console.ReadKey();
+Console.WriteLine(keyInfo.KeyChar);
+Console.WriteLine(keyInfo.Key);
+if(keyInfo.Key == ConsoleKey.Tab)
+{
+    Console.WriteLine("Tab tuşu");
+}
+
+
+switch (keyInfo.Key)
+{
+    case ConsoleKey.W:
+        Console.WriteLine("ileri");
+        break;
+    case ConsoleKey.S:
+        Console.WriteLine("geri");
+        break;
+    case ConsoleKey.D:
+        Console.WriteLine("sağa");
+        break;
+    case ConsoleKey.A:
+        Console.WriteLine("sola");
+        break;
+    default:
+        break;
+}
+
+
+Console.ReadKey();
+
---
+++ C#/dorduncuders/20230405/02_Switch_Expression/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using System.Threading.Channels;
+
+string isim = "platon";
+
+
+//isim değişkeni platon içeriyor mu ?
+if(isim is "platon")
+{
+
+}
+
+//isim değişkeni platon içermiyor mu ? 
+if(isim is not "platon")
+{
+
+}
+
+// koşullu switch case'de sıralama önemlidir.
+int number = 10;
+switch (number)
+{
+    case 0:
+        Console.WriteLine("Sıfı");
+        break;
+    case < 5:
+        Console.WriteLine("5'ten küçüktür");
+        break;
+    case >= 15:
+        Console.WriteLine("15'ten büyüktür ve eşittir.");
+        break;
+}
+
+int number1 = 2;
+//Switch expression
+string numberText = number1 switch
+{
+    1 => "bir",
+    2 => "iki",
+    3 => "üç",
+    _ => "hatalı değer"
+};
+Console.WriteLine(numberText);
+
+Console.WriteLine(number switch
+{
+    1 => "bir",
+    2 => "iki",
+    _ => "hatalıdeğer"
+}) ;
+
+string isim1 = "platon";
+string sonuc = isim switch
+{
+    "platon" or "aristo" => "Tamam"
+};
+
+int number2 = 1;
+string result = number2 switch
+{
+    1 or 2 => "1veya 2",
+    >= 3 and <= 7 => "3 ile 7 arasında",
+    not 9 => "9 değil",
+    _=>  "Tanımsız"
+};
+Console.WriteLine(result);
+
+
+string result3 = DateTime.Now.DayOfWeek switch
+{
+    DayOfWeek.Wednesday when DateTime.Now.Hour > 20 => "Çarşamba akşam",
+    DayOfWeek.Wednesday => "Çarşamba"
+};
+Console.WriteLine(result3);
+
+
+
+
+Console.ReadKey();
