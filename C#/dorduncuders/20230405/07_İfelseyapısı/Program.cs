1416aa5f7837e6215e62e88cef8a560e56829f8f
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-04-06 17:08:05 +0300

    Add files via upload

---
+++ C#/dorduncuders/20230405/07_İfelseyapısı/Program.cs
@@ -0,0 +1,49 @@
+﻿int sayi = 10;
+
+if (sayi % 2 == 0)
+{
+    Console.WriteLine($"{sayi} sayısı çifttir.");
+}
+else
+{
+    Console.WriteLine($"{sayi} sayısı tektir.");
+}
+
+int number = int.Parse(Console.ReadLine());
+if (number > 0)
+{
+    Console.WriteLine("Pozitif");
+}
+else
+{
+    
+    if (number < 0)
+    {
+
+        Console.WriteLine("Sayi negatif");
+    }
+    else
+    {
+        Console.WriteLine("Sıfıra eşittir");
+    }
+}
+
+int number1 = int.Parse(Console.ReadLine());
+
+if (number1 > 0)
+{
+    Console.WriteLine("Pozitif");
+}
+else if (sayi == 0)
+{
+    Console.WriteLine("Sıfır");
+}
+else
+{
+    Console.WriteLine("Negatif");
+}
+
+
+
+
+Console.ReadKey();
